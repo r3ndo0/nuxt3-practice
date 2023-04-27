72a97cd80768aa829e28d61d79cacabc5274807a
r3ndo0 <r3ndo0@gmail.com>
2023-04-27 21:59:20 +0430

    custom hook

---
+++ pages/test3.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+
+const { data, pending, error } = await useAsyncData("test", () => privateFetch.get("bearer"))
+
+
+
+console.log(data.value, pending.value, error.value)
+
+
+
+
+</script>
+
+
+
+<template>
+    <div>
+        <p v-if="pending">Loading...</p>
+
+    </div>
+</template>
